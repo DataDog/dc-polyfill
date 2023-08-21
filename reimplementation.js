ee757b1096593afb0f54e552f463ba8eb3e6d881
Thomas Hunter II <tlhunter@datadog.com>
2023-08-21 13:11:14 -0700

    first crack at TracingChannel

--- reimplementation.js
+++ reimplementation.js
@@ -3,51 +3,7 @@
 // https://github.com/simon-id/diagnostics_channel-polyfill
 
 const util = require('util');
-
-// Port from node core lib/internal/errors.js
-class ERR_INVALID_ARG_TYPE extends TypeError {
-  constructor(message, actual) {
-    super();
-
-    if (actual == null) {
-      message += `. Received ${actual}`;
-    } else if (typeof actual === 'function' && actual.name) {
-      message += `. Received function ${actual.name}`;
-    } else if (typeof actual === 'object') {
-      if (actual.constructor && actual.constructor.name) {
-        message += `. Received an instance of ${actual.constructor.name}`;
-      } else {
-        const inspected = util.inspect(actual, { depth: -1 });
-        message += `. Received ${inspected}`;
-      }
-    } else {
-      let inspected = util.inspect(actual, { colors: false });
-      if (inspected.length > 25) {
-        inspected = `${inspected.slice(0, 25)}...`;
-      }
-      message += `. Received type ${typeof actual} (${inspected})`;
-    }
-
-    this.code = this.constructor.name;
-
-    Object.defineProperties(this, {
-      message: {
-        value: message,
-        enumerable: false,
-        writable: true,
-        configurable: true,
-      },
-      toString: {
-        value() {
-          return `${this.name} [${this.code}]: ${this.message}`;
-        },
-        enumerable: false,
-        writable: true,
-        configurable: true,
-      },
-    });
-  }
-}
+const { ERR_INVALID_ARG_TYPE } = require('./errors.js');
 
 class ActiveChannel {
   subscribe(subscription) {
