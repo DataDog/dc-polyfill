c017c441de27bd107d990294e5c2445a33a3a7f5
Thomas Hunter II <tlhunter@datadog.com>
2023-09-27 15:54:07 -0700

    rebuild test suite runner otherwise tests pollute each other

---
+++ test.sh
@@ -0,0 +1,13 @@
+echo "Node.js version: $(node --version)"
+
+errors=0
+
+for i in test/*.js; do
+	node "$i" || ((errors++))
+done
+
+if (($errors > 0)); then
+	echo "Encountered ${errors} test suite failures!"
+fi
+
+exit $errors
