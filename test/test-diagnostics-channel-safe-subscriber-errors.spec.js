7b8cfac5b2c786cec543a48a131a617b542f0c91
Thomas Hunter II <tlhunter@datadog.com>
2023-10-03 14:41:33 -0700

    test/test-diagnostics-channel-safe-subscriber-errors.spec.js

---
+++ test/test-diagnostics-channel-safe-subscriber-errors.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const test = require('tape');
+const common = require('./common.js');
+const dc = require('../dc-polyfill.js');
+
+const input = {
+  foo: 'bar'
+};
+
+test('test-diagnostics-channel-http', (t) => {
+  t.plan(1);
+  const channel = dc.channel('fail');
+
+  const error = new Error('nope');
+
+  process.on('uncaughtException', common.mustCall((err) => {
+    t.strictEqual(err, error);
+  }));
+
+  channel.subscribe(common.mustCall((message, name) => {
+    throw error;
+  }));
+
+  // The failing subscriber should not stop subsequent subscribers from running
+  channel.subscribe(common.mustCall());
+
+  // Publish should continue without throwing
+  const fn = common.mustCall();
+  channel.publish(input);
+  fn();
+});
