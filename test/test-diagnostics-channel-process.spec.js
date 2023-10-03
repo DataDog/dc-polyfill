d034338b3d45b0379108351daa0a44a44c3ae496
Thomas Hunter II <tlhunter@datadog.com>
2023-10-03 14:39:28 -0700

    test/test-diagnostics-channel-process.js

---
+++ test/test-diagnostics-channel-process.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const dc = require('../dc-polyfill.js');
+const common = require('./common.js');
+const cluster = require('cluster');
+
+const { MAJOR } = require('../checks.js');
+if (MAJOR < 20) return;
+
+if (cluster.isPrimary) {
+  const test = require('tape');
+  const { ChildProcess } = require('child_process');
+  test('test-diagnostics-channel-process', (t) => {
+    t.plan(1);
+    dc.subscribe('child_process', common.mustCall(({ process }) => {
+      t.strictEqual(process instanceof ChildProcess, true);
+    }));
+    const worker = cluster.fork();
+    worker.on('online', common.mustCall(() => {
+      worker.send('disconnect');
+    }));
+  });
+} else {
+  const assert = require('assert');
+  process.on('message', common.mustCall((msg) => {
+    assert.strictEqual(msg, 'disconnect');
+    process.disconnect();
+  }));
+}
