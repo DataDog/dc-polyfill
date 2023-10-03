a02f70a1a377331702ed907fe816b8db5ace26de
Thomas Hunter II <tlhunter@datadog.com>
2023-10-03 11:11:34 -0700

    tracing channel support

--- patch-tracing-channel.js
+++ patch-tracing-channel.js
@@ -1,168 +1,179 @@
 console.log('PATCH-TRACING-CHANNEL');
 
 const ReflectApply = Reflect.apply;
-const PromiseReject = Promise.reject;
+const PromiseReject = Promise.reject.bind(Promise);
 const PromiseResolve = Promise.resolve;
 const makeCall = (fn) => (...args) => fn.call(...args);
 const PromisePrototypeThen = makeCall(Promise.prototype.then);
 const ArrayPrototypeSplice = makeCall(Array.prototype.splice);
+const ArrayPrototypeAt = makeCall(Array.prototype.at);
 
 const { ERR_INVALID_ARG_TYPE } = require('./errors.js');
 
-class TracingChannel {
-  constructor(nameOrChannels) {
-    if (typeof nameOrChannels === 'string') {
-      this.start = channel(`tracing:${nameOrChannels}:start`);
-      this.end = channel(`tracing:${nameOrChannels}:end`);
-      this.asyncStart = channel(`tracing:${nameOrChannels}:asyncStart`);
-      this.asyncEnd = channel(`tracing:${nameOrChannels}:asyncEnd`);
-      this.error = channel(`tracing:${nameOrChannels}:error`);
-    } else if (typeof nameOrChannels === 'object') {
-      const { start, end, asyncStart, asyncEnd, error } = nameOrChannels;
+const traceEvents = [
+  'start',
+  'end',
+  'asyncStart',
+  'asyncEnd',
+  'error',
+];
 
-      // assertChannel(start, 'nameOrChannels.start');
-      // assertChannel(end, 'nameOrChannels.end');
-      // assertChannel(asyncStart, 'nameOrChannels.asyncStart');
-      // assertChannel(asyncEnd, 'nameOrChannels.asyncEnd');
-      // assertChannel(error, 'nameOrChannels.error');
+module.exports = function (dc) {
+  const { channel } = dc;
 
-      this.start = start;
-      this.end = end;
-      this.asyncStart = asyncStart;
-      this.asyncEnd = asyncEnd;
-      this.error = error;
-    } else {
-      throw new ERR_INVALID_ARG_TYPE('nameOrChannels',
-                                     ['string', 'object', 'Channel'],
-                                     nameOrChannels);
-    }
-  }
+  class TracingChannel {
+    constructor(nameOrChannels) {
+      if (typeof nameOrChannels === 'string') {
+        this.start = channel(`tracing:${nameOrChannels}:start`);
+        this.end = channel(`tracing:${nameOrChannels}:end`);
+        this.asyncStart = channel(`tracing:${nameOrChannels}:asyncStart`);
+        this.asyncEnd = channel(`tracing:${nameOrChannels}:asyncEnd`);
+        this.error = channel(`tracing:${nameOrChannels}:error`);
+      } else if (typeof nameOrChannels === 'object') {
+        const { start, end, asyncStart, asyncEnd, error } = nameOrChannels;
 
-  subscribe(handlers) {
-    for (const name of traceEvents) {
-      if (!handlers[name]) continue;
+        // assertChannel(start, 'nameOrChannels.start');
+        // assertChannel(end, 'nameOrChannels.end');
+        // assertChannel(asyncStart, 'nameOrChannels.asyncStart');
+        // assertChannel(asyncEnd, 'nameOrChannels.asyncEnd');
+        // assertChannel(error, 'nameOrChannels.error');
 
-      if (this[name]) this[name].subscribe(handlers[name]);
-    }
-  }
-
-  unsubscribe(handlers) {
-    let done = true;
-
-    for (const name of traceEvents) {
-      if (!handlers[name]) continue;
-
-      if (!(this[name] && this[name].unsubscribe(handlers[name]))) {
-        done = false;
-      }
-    }
-
-    return done;
-  }
-
-  traceSync(fn, context = {}, thisArg, ...args) {
-    const { start, end, error } = this;
-
-    return start.runStores(context, () => {
-      try {
-        const result = ReflectApply(fn, thisArg, args);
-        context.result = result;
-        return result;
-      } catch (err) {
-        context.error = err;
-        error.publish(context);
-        throw err;
-      } finally {
-        end.publish(context);
-      }
-    });
-  }
-
-  tracePromise(fn, context = {}, thisArg, ...args) {
-    const { start, end, asyncStart, asyncEnd, error } = this;
-
-    function reject(err) {
-      context.error = err;
-      error.publish(context);
-      asyncStart.publish(context);
-      // TODO: Is there a way to have asyncEnd _after_ the continuation?
-      asyncEnd.publish(context);
-      return PromiseReject(err);
-    }
-
-    function resolve(result) {
-      context.result = result;
-      asyncStart.publish(context);
-      // TODO: Is there a way to have asyncEnd _after_ the continuation?
-      asyncEnd.publish(context);
-      return result;
-    }
-
-    return start.runStores(context, () => {
-      try {
-        let promise = ReflectApply(fn, thisArg, args);
-        // Convert thenables to native promises
-        if (!(promise instanceof Promise)) {
-          promise = PromiseResolve(promise);
-        }
-        return PromisePrototypeThen(promise, resolve, reject);
-      } catch (err) {
-        context.error = err;
-        error.publish(context);
-        throw err;
-      } finally {
-        end.publish(context);
-      }
-    });
-  }
-
-  traceCallback(fn, position = -1, context = {}, thisArg, ...args) {
-    const { start, end, asyncStart, asyncEnd, error } = this;
-
-    function wrappedCallback(err, res) {
-      if (err) {
-        context.error = err;
-        error.publish(context);
+        this.start = start;
+        this.end = end;
+        this.asyncStart = asyncStart;
+        this.asyncEnd = asyncEnd;
+        this.error = error;
       } else {
-        context.result = res;
+        throw new ERR_INVALID_ARG_TYPE('nameOrChannels',
+                                       ['string', 'object', 'Channel'],
+                                       nameOrChannels);
+      }
+    }
+
+    subscribe(handlers) {
+      for (const name of traceEvents) {
+        if (!handlers[name]) continue;
+
+        if (this[name]) this[name].subscribe(handlers[name]);
+      }
+    }
+
+    unsubscribe(handlers) {
+      let done = true;
+
+      for (const name of traceEvents) {
+        if (!handlers[name]) continue;
+
+        if (!(this[name] && this[name].unsubscribe(handlers[name]))) {
+          done = false;
+        }
       }
 
-      // Using runStores here enables manual context failure recovery
-      asyncStart.runStores(context, () => {
+      return done;
+    }
+
+    traceSync(fn, context = {}, thisArg, ...args) {
+      const { start, end, error } = this;
+
+      return start.runStores(context, () => {
         try {
-          if (callback) {
-            return ReflectApply(callback, this, arguments);
-          }
+          const result = ReflectApply(fn, thisArg, args);
+          context.result = result;
+          return result;
+        } catch (err) {
+          context.error = err;
+          error.publish(context);
+          throw err;
         } finally {
-          asyncEnd.publish(context);
+          end.publish(context);
         }
       });
     }
 
-    const callback = ArrayPrototypeAt(args, position);
-    if (typeof callback !== 'function') {
-      throw new ERR_INVALID_ARG_TYPE('callback', ['function'], callback);
-    }
-    ArrayPrototypeSplice(args, position, 1, wrappedCallback);
+    tracePromise(fn, context = {}, thisArg, ...args) {
+      const { start, end, asyncStart, asyncEnd, error } = this;
 
-    return start.runStores(context, () => {
-      try {
-        return ReflectApply(fn, thisArg, args);
-      } catch (err) {
+      function reject(err) {
         context.error = err;
         error.publish(context);
-        throw err;
-      } finally {
-        end.publish(context);
+        asyncStart.publish(context);
+        // TODO: Is there a way to have asyncEnd _after_ the continuation?
+        asyncEnd.publish(context);
+        return PromiseReject(err);
       }
-    });
+
+      function resolve(result) {
+        context.result = result;
+        asyncStart.publish(context);
+        // TODO: Is there a way to have asyncEnd _after_ the continuation?
+        asyncEnd.publish(context);
+        return result;
+      }
+
+      return start.runStores(context, () => {
+        try {
+          let promise = ReflectApply(fn, thisArg, args);
+          // Convert thenables to native promises
+          if (!(promise instanceof Promise)) {
+            promise = PromiseResolve(promise);
+          }
+          return PromisePrototypeThen(promise, resolve, reject);
+        } catch (err) {
+          context.error = err;
+          error.publish(context);
+          throw err;
+        } finally {
+          end.publish(context);
+        }
+      });
+    }
+
+    traceCallback(fn, position = -1, context = {}, thisArg, ...args) {
+      const { start, end, asyncStart, asyncEnd, error } = this;
+
+      function wrappedCallback(err, res) {
+        if (err) {
+          context.error = err;
+          error.publish(context);
+        } else {
+          context.result = res;
+        }
+
+        // Using runStores here enables manual context failure recovery
+        asyncStart.runStores(context, () => {
+          try {
+            if (callback) {
+              return ReflectApply(callback, this, arguments);
+            }
+          } finally {
+            asyncEnd.publish(context);
+          }
+        });
+      }
+
+      const callback = ArrayPrototypeAt(args, position);
+      if (typeof callback !== 'function') {
+        throw new ERR_INVALID_ARG_TYPE('callback', ['function'], callback);
+      }
+      ArrayPrototypeSplice(args, position, 1, wrappedCallback);
+
+      return start.runStores(context, () => {
+        try {
+          return ReflectApply(fn, thisArg, args);
+        } catch (err) {
+          context.error = err;
+          error.publish(context);
+          throw err;
+        } finally {
+          end.publish(context);
+        }
+      });
+    }
   }
-}
 
-function tracingChannel(nameOrChannels) {
-  return new TracingChannel(nameOrChannels);
-}
+  function tracingChannel(nameOrChannels) {
+    return new TracingChannel(nameOrChannels);
+  }
 
-module.exports = function (dc) {
   dc.tracingChannel = tracingChannel;
 };
