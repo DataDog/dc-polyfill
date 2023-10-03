e811ac4a3f146e30995d7bd81a45cc929d484c43
Thomas Hunter II <tlhunter@datadog.com>
2023-10-03 15:39:38 -0700

    test/test-diagnostics-channel-worker-threads.spec.js

---
+++ test/test-diagnostics-channel-worker-threads.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+const test = require('tape');
+const common = require('./common.js');
+const dc = require('../dc-polyfill.js');
+const { Worker } = require('worker_threads');
+const { MAJOR, MINOR } = require('../checks.js');
+
+// This test depends on features only available in certain versions of Node.js
+if (MAJOR < 19) return;
+
+test('test-diagnostics-channel-worker-threads', (t) => {
+  t.plan(1);
+
+  dc.subscribe('worker_threads', common.mustCall(({ worker }) => {
+    t.strictEqual(worker instanceof Worker, true);
+  }));
+
+  new Worker('const a = 1;', { eval: true });
+});
