9575bed9ef929231e50ba6117ac5d594e60582dc
Bryan English <bryan.english@datadoghq.com>
2025-03-21 13:47:55 -0400

    make it faster

--- test/esm.spec.js
+++ test/esm.spec.js
@@ -3,52 +3,30 @@ const fs = require('fs');
 const os = require('os');
 const execSync = require('child_process').execSync;
 
-test('esm basic import', t => {
+const polyfillDir = process.cwd();
+let basedir;
+
+test('create fixture project directory', t => {
   const tmpdir = os.tmpdir();
-  const basedir = fs.mkdtempSync(`${tmpdir}/esm-basic-`);
-  const polyfillDir = process.cwd();
+  basedir = fs.mkdtempSync(`${tmpdir}/esm-`);
   process.chdir(basedir);
   execSync('npm init -y');
   execSync('npm install ' + polyfillDir);
-  fs.writeFileSync('esm.mjs', `import { tracingChannel } from 'dc-polyfill'`);
-  t.doesNotThrow(() => execSync('node esm.mjs'));
-  process.chdir(polyfillDir);
-  execSync('rm -rf ' + basedir);
+  execSync('cp -r ' + polyfillDir + '/test/fixtures/* .');
   t.end();
 });
 
 test('esm default export', t => {
-  const tmpdir = os.tmpdir();
-  const basedir = fs.mkdtempSync(`${tmpdir}/esm-default-`);
-  const polyfillDir = process.cwd();
-  process.chdir(basedir);
-  execSync('npm init -y');
-  execSync('npm install ' + polyfillDir);
-  
-  // Create a test file that uses the default export
-  fs.copyFileSync(`${polyfillDir}/test/fixtures/esm-default.mjs`, 'esm-default.mjs');
-  
   t.doesNotThrow(() => execSync('node esm-default.mjs'));
-  process.chdir(polyfillDir);
-  execSync('rm -rf ' + basedir);
   t.end();
 });
 
 test('esm named exports', t => {
-  const tmpdir = os.tmpdir();
-  const basedir = fs.mkdtempSync(`${tmpdir}/esm-named-`);
-  const polyfillDir = process.cwd();
-  process.chdir(basedir);
-  execSync('npm init -y');
-  execSync('npm install ' + polyfillDir);
-  
-  // Create a test file that imports named exports individually
-  fs.copyFileSync(`${polyfillDir}/test/fixtures/esm-named.mjs`, 'esm-named.mjs');
-  fs.writeFileSync('esm-named.mjs', `
-
-  `);
-  
   t.doesNotThrow(() => execSync('node esm-named.mjs'));
+  t.end();
+});
+
+test('cleanup', t => {
   process.chdir(polyfillDir);
   execSync('rm -rf ' + basedir);
   t.end();
